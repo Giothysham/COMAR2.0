2b96a40f5de3a8be10387f1516aaad94b2775eea
giothysham <edmond.tsampanis@student.uhasselt.be>
2023-11-16 10:43:11 +0100

    test
    Signed-off-by: giothysham <edmond.tsampanis@student.uhasselt.be>

---
+++ COMAR/RISC-V/RISC-V.gen/sources_1/bd/design_3/hdl/design_3_wrapper.v
@@ -0,0 +1,19 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2023.1 (win64) Build 3865809 Sun May  7 15:05:29 MDT 2023
+//Date        : Thu Nov  9 09:20:50 2023
+//Host        : edmond running 64-bit major release  (build 9200)
+//Command     : generate_target design_3_wrapper.bd
+//Design      : design_3_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_3_wrapper
+   ();
+
+
+  design_3 design_3_i
+       ();
+endmodule
---
+++ COMAR/RISC-V/RISC-V.gen/sources_1/bd/design_3/synth/design_3.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2023.1 (win64) Build 3865809 Sun May  7 15:05:29 MDT 2023
+//Date        : Thu Nov  9 09:20:50 2023
+//Host        : edmond running 64-bit major release  (build 9200)
+//Command     : generate_target design_3.bd
+//Design      : design_3
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+(* CORE_GENERATION_INFO = "design_3,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=design_3,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=0,numReposBlks=0,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=0,numPkgbdBlks=0,bdsource=USER,synth_mode=OOC_per_IP}" *) (* HW_HANDOFF = "design_3.hwdef" *) 
+module design_3
+   ();
+
+
+endmodule
